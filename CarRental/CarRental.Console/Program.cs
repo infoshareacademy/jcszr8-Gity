955a85f28053b6fa7fd1a4ebe98a09513ee03cbd
janek2j <janek2j@gmail.com>
2022-12-11 21:12:41 +0100

    add: menu

--- CarRental/CarRental.Console/Program.cs
+++ CarRental/CarRental.Console/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(@"Menu:
+1. Pokaż listę samochodów.
+2. Wyszukaj samochód po roczniku.
+3. Wyszukaj samochód po dacie dostępności.
+4. Wyszukaj samochód do wypożyczenia po nazwie.
+5. Wyszukaj samochód po wyposażeniu.
+6. Dodaj samochód.
+7. Pokaż wypożyczenia.
+8. Dodaj wypożeczenie.
+9. Edytuj wypożyczenie.
+10. Edytuj dane samochodu.
+");
+           
         }
     }
 }
