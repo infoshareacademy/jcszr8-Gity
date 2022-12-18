ce86bd578366458755e14ac36488a30285139a54
janek2j <janek2j@gmail.com>
2022-12-18 14:55:15 +0100

    add: DataReaderAndWriter class; this class has method for getting list of cars from Json file

--- CarRental/CarRental.Console/Program.cs
+++ CarRental/CarRental.Console/Program.cs
@@ -6,7 +6,16 @@ namespace CarRental.ConsoleUI
     {
         static void Main(string[] args)
         {
-            Menu();
+            DataReaderAndWriter.WriteCarsToJson();
+
+            List<Car> cars = DataReaderAndWriter.GetCarsFromJson();
+            //Console.WriteLine($"Number of cars in database: {cars.Count}");
+            //foreach (Car car in cars)
+            //{
+            //    Console.WriteLine(car.Model);
+            //}
+
+            Menu();   
         }
 
 
