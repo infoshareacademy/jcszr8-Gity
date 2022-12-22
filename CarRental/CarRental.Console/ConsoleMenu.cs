355afa28804d3822c63043c14f4d0deac28276e4
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2022-12-22 05:38:33 +0100

    Create ConsoleMenu.cs

---
+++ CarRental/CarRental.Console/ConsoleMenu.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.ConsoleUI
+{
+    public class ConsoleMenu
+    {
+        private static readonly Dictionary<ConsoleKey, string> _menuOptions = new()
+        {
+            {ConsoleKey.D1, "Pokaż listę samochodów."},
+            {ConsoleKey.D2, "Wyszukaj samochód po roczniku."},
+            {ConsoleKey.D3, "Wyszukaj samochód po nazwie."},
+            {ConsoleKey.D4, "Wyszukaj samochód po dacie dostępności."},
+            {ConsoleKey.D5, "Wyszukaj samochód do wypożyczenia po nazwie."},
+            {ConsoleKey.D6, "Wyszukaj samochód po wyposażeniu."},
+            {ConsoleKey.D7, "Dodaj samochód."},
+            {ConsoleKey.D8, "Pokaż wypożyczenia."},
+            {ConsoleKey.D9, "Dodaj wypożeczenie."},
+            {ConsoleKey.D0, "Edytuj wypożyczenie."},
+            {ConsoleKey.F1, "Edytuj dane samochodu."},
+            {ConsoleKey.Escape, "Wyjdź"},
+
+        };
+        public static void Menu()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Witaj w naszej wypożyczalni");
+                Console.WriteLine("Choose option by writing the number:");
+
+                for (int i = 0; i < _menuOptions.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {_menuOptions.ElementAt(i).Value}");
+                }
+                ConsoleKeyInfo read = Console.ReadKey();
+                Console.WriteLine();
+                switch (read.Key)
+                {
+                    case ConsoleKey.D1:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D2:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D3:
+                        Confirmation(); Console.Clear(); Search.CarByName();
+                        break;
+                    case ConsoleKey.D4:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D5:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D6:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D7:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D8:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D9:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.D0:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.F1:
+                        Confirmation(); Console.Clear(); Search.PlaceHolder();
+                        break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                    default: break;
+                }
+
+            }
+
+        }
+
+        public static void Confirmation()
+        {
+            Console.WriteLine("Czy napewno chcesz wybrac ta opcje?: T/N");
+            ConsoleKeyInfo conf = Console.ReadKey();
+            while (true)
+            {
+                if (conf.Key == ConsoleKey.T) break;
+                else if (conf.Key == ConsoleKey.N) Menu();
+                else
+                    Console.WriteLine();
+                Console.WriteLine("Nieprawidłowa komenda");
+                Thread.Sleep(600);
+                Menu();
+            }
+        }
+    }
+}
--- CarRental/CarRental.Console/Program.cs
+++ CarRental/CarRental.Console/Program.cs
@@ -14,89 +14,10 @@ namespace CarRental.ConsoleUI
             //{
             //    Console.WriteLine(car.Model);
             //}
+            Console.Title = "Cud Auta";
+            ConsoleMenu.Menu();
 
-            Menu();   
         }
 
-
-        static void Menu()
-        {
-            Console.Title = "Wypożyczalnia aut";
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("Menu");
-                Console.WriteLine("1 - Pokaż listę samochodów.");
-                Console.WriteLine("2 - Wyszukaj samochód po roczniku.");
-                Console.WriteLine("3 - Wyszukaj samochód po nazwie.");
-                Console.WriteLine("4 - Wyszukaj samochód po dacie dostępności.");
-                Console.WriteLine("5 - Wyszukaj samochód do wypożyczenia po nazwie.");
-                Console.WriteLine("6 - Wyszukaj samochód po wyposażeniu.");
-                Console.WriteLine("7 - Dodaj samochód.");
-                Console.WriteLine("8 - Pokaż wypożyczenia.");
-                Console.WriteLine("9 - Dodaj wypożeczenie.");
-                Console.WriteLine("0 - Edytuj wypożyczenie.");
-                Console.WriteLine("F1 - Edytuj dane samochodu.");
-                ConsoleKeyInfo read = Console.ReadKey();
-                Console.WriteLine();
-                Console.WriteLine("Czy napewno chcesz wybrac ta opcje?: Tak/Nie");
-                string conf = Console.ReadLine();
-                if (conf == "Tak")
-                {
-                    switch (read.Key)
-                    {
-                        case ConsoleKey.D1:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D2:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D3:
-                            Console.Clear(); SearchCarByName();
-                            break;
-                        case ConsoleKey.D4:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D5:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D6:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D7:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D8:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D9:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.D0:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        case ConsoleKey.F1:
-                            Console.Clear(); PlaceHOlder();
-                            break;
-                        default: break;
-                    }
-                }
-                else continue;
-            }
-        }
-
-        public static Car SearchCarByName()
-        {
-            Console.WriteLine("tak");
-            Console.ReadKey();
-            return null;
-        }
-
-        //Zastępstwo za przyszłe funkcje
-        static void PlaceHOlder()
-        {
-            Console.WriteLine("PlaceHolder");
-            Console.ReadKey();
-        }
     }
 }
