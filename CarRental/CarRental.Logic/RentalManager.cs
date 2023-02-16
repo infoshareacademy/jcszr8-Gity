f56245d5965152c35e5c3e911764a7442ce2a23f
Jacek Pardel <jacekpardel@gmail.com>
2023-02-16 21:31:24 +0100

    working progress

---
+++ CarRental/CarRental.Logic/RentalManager.cs
@@ -0,0 +1,29 @@
+﻿using CarRental.DAL.Models;
+using CarRental.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Logic
+{
+    public class RentalManager
+    {
+        private static readonly int _idCounter = CarRentalData.Rentals.Max(r => r.Id);
+
+        private static List<Rental> _rentals = CarRentalData.Rentals;
+        
+        public static Rental GetById(int rentalId)
+        {
+            return _rentals.FirstOrDefault(r => r.Id == rentalId);
+        }
+
+        //private static int GetNextId()
+        //{
+        //    return ++_idCounter;
+        //}
+
+    }
+   
+}
---
+++ CarRental/CarRental.Logic/Search.cs
@@ -0,0 +1,89 @@
+﻿using CarRental.DAL;
+using CarRental.DAL.Models;
+using CarRental.DAL.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Logic
+{
+    public class Search
+    {
+
+        public static List<Rental> CarByMake(string make)
+        {
+            List<Rental> cars = new List<Rental>();
+            if (string.IsNullOrEmpty(make))
+            {
+                cars = CarRentalData.Cars;
+            }
+            else
+            {
+                cars = CarRentalData.Cars.Where(c => c.Make?.ToLower() == make.ToLower() ||
+                                                     c.CarModel?.ToLower() == make.ToLower()).ToList();
+
+            }
+            return cars;
+        }
+
+        public static List<Rental> CarByName(string name)
+        {
+            List<Rental> cars = new List<Rental>();
+            if (string.IsNullOrEmpty(name))
+            {
+                cars = CarRentalData.Cars;
+            }
+            else
+            {
+
+                cars = CarRentalData.Cars.Where(c => c.Make.Contains(name, StringComparison.CurrentCultureIgnoreCase)
+                    || c.CarModel.Contains(name, StringComparison.CurrentCultureIgnoreCase)
+                ).ToList();
+            }
+            return cars;
+        }
+
+        public static List<Rental> CarByYear(string read)
+        {
+            int year;
+            bool makes = int.TryParse(read, out year);
+            List<Rental> cars = new List<Rental>();
+            if (read == null)
+            {
+                cars = CarRentalData.Cars;
+            }
+            else
+            {
+                cars = CarRentalData.Cars.Where(c => c.Year == year).ToList();
+            }
+            return cars;
+        }
+
+        public static List<Rental> CarByAddons(string addon)
+        {
+            List<Rental> cars = new List<Rental>();
+            if (string.IsNullOrEmpty(addon))
+            {
+                cars = CarRentalData.Cars;
+            }
+            else
+            {
+                foreach (var car in CarRentalData.Cars)
+                {
+                    foreach (var item in car.Addons)
+                    {
+                        if (item.Contains(addon))
+                        {
+                            cars.Add(car);
+                            break;
+                        }
+                    }
+                }
+                //cars = CarRentalData.Cars.Where(c => c.Addons.Where(x => x.Contains(addon)).FirstOrDefault()
+            }
+            return cars;
+        }
+    }
+}
