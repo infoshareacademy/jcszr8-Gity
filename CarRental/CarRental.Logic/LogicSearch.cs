1d26c35c303e2bc337971cd3bf9cb54a1fb35d7b
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-02-18 10:55:08 +0100

    Update LogicSearch.cs

--- CarRental/CarRental.Logic/LogicSearch.cs
+++ CarRental/CarRental.Logic/LogicSearch.cs
@@ -88,12 +88,12 @@ namespace CarRental.Logic
         public static string Print(List<Car> cars)
         {
             StringBuilder sb = new();
-            sb.Append(String.Format("\n{0,4}.| {1,-20}| {2,-25}| {3,-10}| {4,-20}\n", "Id", "Make", "Model", "License plate", "Addons"));
+            sb.Append(String.Format("\n{0,4}.| {1,-20}| {2,-25}| {3,-20}| {4,-20}\n", "Id", "Make", "Model", "License plate", "Addons"));
             sb.Append(new String('-', sb.Length));
             sb.Append('\n');
             foreach (var car in cars)
             {
-                sb.Append($"{car.Id,4}.| {car.Make,-20}| {car.CarModel,-25}| {car.LicencePlateNumber,-20}| {car.GetAddonsToString(),10}{Environment.NewLine}");
+                sb.Append($"{car.Id,4}.| {car.Make,-20}| {car.CarModel,-25}| {car.LicencePlateNumber,-20}| {car.GetAddonsToString(),-20}{Environment.NewLine}");
             }
             return sb.ToString();
         }
