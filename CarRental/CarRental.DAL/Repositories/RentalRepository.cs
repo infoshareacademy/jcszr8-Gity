6ec85c624e488a43d00b51cae413c638ea373961
janek2j <janek2j@gmail.com>
2023-03-12 20:36:14 +0100

    add empty CarRepository and RentalRepository classes

---
+++ CarRental/CarRental.DAL/Repositories/RentalRepository.cs
@@ -0,0 +1,31 @@
+﻿using CarRental.DAL.Models;
+
+namespace CarRental.DAL.Repositories;
+
+public class RentalRepository : IRepository<Rental> // TODO RentalRepository
+{
+    public void Add(Rental entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Rental entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Rental> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Rental GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Rental entity)
+    {
+        throw new NotImplementedException();
+    }
+}
