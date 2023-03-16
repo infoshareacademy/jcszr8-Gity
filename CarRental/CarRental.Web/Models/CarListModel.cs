ff5406306b2f7810c5b073d2251370366e6fa844
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-03-16 21:03:30 +0100

    Update CarListModel.cs

--- CarRental/CarRental.Web/Models/CarListModel.cs
+++ CarRental/CarRental.Web/Models/CarListModel.cs
@@ -1,19 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CarRental.DAL.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace CarRental.Web.Models
+namespace CarRental.Web.Models;
+
+public class CarListModel
 {
-    public class CarListModel
+    public int Id { get; set; }
+
+    [Display(Name = "Model")]
+    public string? CarModel { get; set; }
+
+    public string? Make { get; set; }
+
+    public int? Year { get; set; }
+
+    [Display(Name = "Licence Plate")]
+    public string? LicencePlateNumber { get; set; }
+    public CarListModel FillModel(Car baseModel)
     {
-        public int Id { get; set; }
-
-        [Display(Name = "Model")]
-        public string? CarModel { get; set; }
-
-        public string? Make { get; set; }
-
-        public int? Year { get; set; }
-
-        [Display(Name = "Licence Plate")]
-        public string? LicencePlateNumber { get; set; }
+        this.Id = baseModel.Id;
+        this.CarModel = baseModel.CarModel;
+        this.Make = baseModel.Make;
+        return this;
     }
 }
