7d79e9f0a5634ab6460230a0d75c3ddf650586d0
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-03-16 22:26:20 +0100

    Update Customer.cs

--- CarRental/CarRental.DAL/Models/Customer.cs
+++ CarRental/CarRental.DAL/Models/Customer.cs
@@ -1,32 +1,45 @@
-﻿using System.Text.Json.Serialization;
-using System.Text;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
 namespace CarRental.DAL.Models;
 public sealed class Customer
 {
-    [Range(0, 100, ErrorMessage = "Zwaliłeś sprawe!")]
     [JsonProperty("id")]
     public int Id;
 
     [JsonProperty("first_name")]
+    [Display(Name = "First Name")]
+    [MaxLength(30)]
+    [Required]
     public string FirstName { get; set; }
 
     [JsonProperty("last_name")]
+    [Display(Name = "Last Name")]
+    [MaxLength(30)]
+    [Required]
     public string LastName { get; set; }
 
     [JsonProperty("email_address")]
+    [Display(Name = "Email")]
+    [EmailAddress]
+    [Required]
+    [MaxLength(50)]
     public string? EmailAddress { get; set; }
 
     [JsonProperty("phone_number")]
+    [Display(Name = "Phone Number")]
+    [Phone]
+    [Required]
     public string PhoneNumber { get; set; }
 
+    [MinLength(11)]
+    [MaxLength(11)]
     [JsonProperty("pesel")]
     public string? Pesel { get; set; }
 
+    [MaxLength(10)]
     [JsonProperty("gender")]
-    public char Gender { get; set; } // F female, M male, O other
+    public char Gender { get; set; }
 
     public Customer(int id, string firstName, string lastName, string phoneNumber)
     {
