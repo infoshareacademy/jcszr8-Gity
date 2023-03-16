e84cf9f3282fe9eb93d7b896b89cb7387236e231
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-03-16 22:27:42 +0100

    Update Car.cs

--- CarRental/CarRental.DAL/Models/Car.cs
+++ CarRental/CarRental.DAL/Models/Car.cs
@@ -1,62 +1,88 @@
-﻿using CarRental.DAL.Enums;
-using Microsoft.VisualBasic;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarRental.DAL.Models;
 public sealed class Car
 {
+    #region Properties/Fields
     public int Id { get; set; }
 
     [JsonProperty(PropertyName = "model")]
     [Display(Name = "Model")]
+    [Required]
     public string? CarModel { get; set; }
 
+    [MaxLength(100)]
+    [Required]
     public string? Make { get; set; }
 
+    // TODO fix end year
+    [Range(2000, 2050,
+       ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int Year { get; set; } // production year
 
+    [MaxLength(20)]
     public string? Color { get; set; }
 
+    [MaxLength(20)]
     public string? Transmission { get; set; }
 
     [JsonProperty("licence_plate_number")]
-    [Display (Name = "Licence Plate")]
+    [Display(Name = "Licence Plate")]
     [Required]
     [MaxLength(8)]
     public string? LicencePlateNumber { get; set; }
 
+    [Range(0, 500_000,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int Kilometrage { get; set; }
 
     [JsonProperty("power_kw")]
+    [Display(Name = "Power in kWs")]
     public float PowerInKiloWats { get; set; }
 
     [JsonProperty("fuel_type")]
+    [Display(Name = "Engine Type")]
     public string EngineType { get; set; }
 
     [JsonProperty("displacement")]
+    [MaxLength(10)]
     public string? Displacement { get; set; } // ex. 1.8, 1.5 T-GDI, etc.
 
+    [Range(3, 10,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int Doors { get; set; }
 
     [JsonProperty("max_capacity")]
+    [Display(Name = "No. of Seats")]
+    [Range(2, 50,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int SeatsNo { get; set; } // total number of seats (with driver seat included)
 
+    [Display(Name = "No. of Airbags")]
+    [Range(0, 10,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int Airbags { get; set; }
 
     [JsonProperty("fuel_consumption")]
-    [Display (Name = "Fuel Consumption")]
+    [Display(Name = "Fuel Consumption")]
     [MaxLength(5)]
     public string? FuelConsumption { get; set; } // in l/100km format city/highway, ex. "6.5/4.5"
 
+    [Display(Name = "Addons")]
     public List<string> Addons { get; set; } = new();
 
+    [Range(100, 1000,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    [Display(Name = "Price/day")]
     public decimal Price { get; set; }
 
     private static List<string> _availableAddons = new() { "Ac", "towbar", "ABS", "roof rack" };
 
     public static List<string> GetAvailableAddons() { return _availableAddons; }
+
+    #endregion
 
     public Car(int id, string make, string model, string licensePlate)
     {
@@ -65,6 +91,8 @@ public sealed class Car
         CarModel = model;
         LicencePlateNumber = licensePlate;
     }
+
+    public Car() { }
 
     public override string ToString()
     {
