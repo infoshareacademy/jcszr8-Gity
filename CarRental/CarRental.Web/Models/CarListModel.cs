53022d5fb694ba5e2f2ef6969f211937974900a4
Jacek Pardel <jacekpardel@gmail.com>
2023-03-16 21:08:56 +0100

    without conflits

--- CarRental/CarRental.Web/Models/CarListModel.cs
+++ CarRental/CarRental.Web/Models/CarListModel.cs
@@ -1,17 +1,21 @@
 ﻿using CarRental.DAL.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace CarRental.Web.Models;
 
 public class CarListModel
 {
     public int Id { get; set; }
 
+    [Display(Name = "Model")]
     public string? CarModel { get; set; }
 
     public string? Make { get; set; }
 
     public int? Year { get; set; }
 
+    [Display(Name = "Licence Plate")]
     public string? LicencePlateNumber { get; set; }
 
     public CarListModel FillModel(Car baseModel)
