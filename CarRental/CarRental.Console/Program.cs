8226a633ceead4bdb72eda02baf95d60af9fb3ae
Jacek Pardel <jacekpardel@gmail.com>
2023-03-16 21:46:27 +0100

    erase class1.cs

--- CarRental/CarRental.Console/Program.cs
+++ CarRental/CarRental.Console/Program.cs
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-﻿using CarRental.DAL;
+using CarRental.DAL;
 
 namespace CarRental.ConsoleUI;
 internal class Program
-=======
-﻿namespace CarRental.ConsoleUI
->>>>>>> origin
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
         Console.Title = AppConfig.APP_NAME;
 
         MainMenu.StartMenu();
-=======
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
->>>>>>> origin
     }
 }
