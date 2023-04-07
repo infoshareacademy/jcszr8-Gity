b4ad43dca5a5a1a9bd60a7eeb1deb7b76bde269d
janek2j <janek2j@gmail.com>
2023-04-07 22:09:18 +0200

    deleted commented experiment (YAGNI)

--- CarRental/CarRental.Web/Models/RentalViewModel.cs
+++ CarRental/CarRental.Web/Models/RentalViewModel.cs
@@ -15,46 +15,4 @@ public class RentalViewModel
 
     [Display(Name = "Car License Plate")]
     public string? CarLicencePlate { get; set; }
-
-
-    //private DateTime _beginDate;
-    //private DateTime _endDate;
-    //private decimal _totaCost;
-
-
-    //public DateTime BeginDate
-    //{
-    //    get
-    //    {
-    //        return _beginDate;
-    //    }
-    //    set
-    //    {
-    //        _beginDate = value < _endDate ? value : _endDate;
-    //    }
-    //}
-
-    //public DateTime EndDate
-    //{
-    //    get
-    //    {
-    //        return _endDate;
-    //    }
-    //    set
-    //    {
-    //        _endDate = value > _beginDate ? value : _beginDate;
-    //    }
-    //}
-
-    //public decimal? TotalCost
-    //{
-    //    get
-    //    {
-    //        return _totaCost;
-    //    }
-    //    set
-    //    {
-    //        TotalCost = value > 0 ? value : 0;
-    //    }
-    //}
 }
