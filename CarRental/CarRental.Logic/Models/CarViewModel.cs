151505a3650c6a1f7e80c430f8733433afc0dc56
Jacek Pardel <jacekpardel@gmail.com>
2023-04-23 18:58:22 +0200

    New label added

--- CarRental/CarRental.Logic/Models/CarViewModel.cs
+++ CarRental/CarRental.Logic/Models/CarViewModel.cs
@@ -12,23 +12,32 @@ public class CarViewModel
     [Required]
     public string? CarModelProp { get; set; }
 
+
     [Required]
     public string? Make { get; set; }
 
+    [Display(Name = "Year Of Production")]
     [Required]
     public int Year { get; set; }
 
+    [Display(Name = "Licence Plate")]
     [Required]
     public string? LicencePlateNumber { get; set; }
     public string? Color { get; set; }
+    [Display(Name = "Transmission Type")]
     public TransmissionType? Transmission { get; set; }
     public int? Kilometrage { get; set; }
+    [Display(Name = "Power In Kilowatts")]
     public float? PowerInKiloWats { get; set; }
+    
+    [Display(Name = "Engine Type")]
     public EngineType? EngineType { get; set; }
     public string? Displacement { get; set; } // ex. 1.8, 1.5 T-GDI, etc.
     public int? Doors { get; set; }
+    [Display(Name = "Number Of Seats")]
     public int? SeatsNo { get; set; } // total number of seats (with driver seat included)
     public int? Airbags { get; set; }
+    [Display(Name = "Fuel Consumption")]
     public string? FuelConsumption { get; set; } // in l/100km format city/highway, ex. "6.5/4.5"
     public List<string> Addons { get; set; } = new();
     public decimal? Price { get; set; }
