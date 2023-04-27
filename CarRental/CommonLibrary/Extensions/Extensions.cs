bc828c726548341ff00192440ef5a032c9031899
Jacek Pardel <jacekpardel@gmail.com>
2023-04-27 22:05:19 +0200

    fixed page with deleting rentals

--- CarRental/CommonLibrary/Extensions/Extensions.cs
+++ CarRental/CommonLibrary/Extensions/Extensions.cs
@@ -10,6 +10,6 @@ public static class Extensions
 {
     public static string DefaultDateFormat(this DateTime dateTime)
     {
-        return Convert.ToDateTime(dateTime).ToString("yyyy-MM-dd hh:mm ");
+        return Convert.ToDateTime(dateTime).ToString("dd-MM-yyyy hh:mm ");
     }
 }
