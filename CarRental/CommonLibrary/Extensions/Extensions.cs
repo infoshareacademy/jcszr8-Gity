4509a23e0ef0b798e59294f410606568cb09633d
janek2j <janek2j@gmail.com>
2023-04-27 17:22:03 +0200

    add Term class; add extension

--- CarRental/CommonLibrary/Extensions/Extensions.cs
+++ CarRental/CommonLibrary/Extensions/Extensions.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarRental.Common.Extensions;
+﻿namespace CarRental.Common.Extensions;
 
 public static class Extensions
 {
     public static string DefaultDateFormat(this DateTime dateTime)
     {
         return Convert.ToDateTime(dateTime).ToString("yyyy-MM-dd hh:mm ");
+    }
+
+    public static string RemoveWhitespaces(this string input)
+    {
+        var temp = input.ToCharArray().Where(c => !char.IsWhiteSpace(c)).ToArray();
+        return new string(temp);
     }
 }
