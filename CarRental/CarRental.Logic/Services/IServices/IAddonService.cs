203681f8ba48b10aeeb9816a7768873c987e995b
janek2j <janek2j@gmail.com>
2023-04-29 14:17:45 +0200

    quick clean - removed  unused IAddonService; removed TODO.txt

--- CarRental/CarRental.Logic/Services/IServices/IAddonService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarRental.Logic.Services.IServices;
-
-public interface IAddonService
-{
-    List<string> GetAddonsAsList(string addons);
-    string GetAddonsAsString(List<string> addons);
-}
