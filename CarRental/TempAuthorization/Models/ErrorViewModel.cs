777ca1154353b1ade2959f5ae321bff31c833127
janek2j <janek2j@gmail.com>
2023-05-05 19:12:57 +0200

    delete TempAuthorization project

--- CarRental/TempAuthorization/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TempAuthorization.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
