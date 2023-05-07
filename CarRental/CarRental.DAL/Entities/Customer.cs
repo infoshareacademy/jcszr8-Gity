95c3b77adeb07d42ad7a08292acbd11dac8715ef
janek2j <janek2j@gmail.com>
2023-05-08 00:29:50 +0200

    clean entities from json annotations

--- CarRental/CarRental.DAL/Entities/Customer.cs
+++ CarRental/CarRental.DAL/Entities/Customer.cs
@@ -1,26 +1,14 @@
 ﻿using CarRental.Common.Enums;
 using CarRental.DAL.Entities.BaseEntity;
-using Newtonsoft.Json;
 
 namespace CarRental.DAL.Entities;
 
 public class Customer : Entity
 {
-    [JsonProperty("first_name")]
     public string FirstName { get; set; }
-
-    [JsonProperty("last_name")]
     public string LastName { get; set; }
-
-    [JsonProperty("phone_number")]
     public string PhoneNumber { get; set; }
-
-    [JsonProperty("email_address")]
     public string? EmailAddress { get; set; }
-
-    [JsonProperty("pesel")]
     public string? Pesel { get; set; }
-
-    [JsonProperty("gender")]
     public Gender Gender { get; set; }
 }
