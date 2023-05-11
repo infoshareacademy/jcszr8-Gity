1c9ff897019300885c9dce4d35621db4efe26fb1
janek2j <janek2j@gmail.com>
2023-05-11 21:36:55 +0200

    WIP

--- CarRental/CarRentalApi/Controllers/ReportController.cs
+++ CarRental/CarRentalApi/Controllers/ReportController.cs
@@ -2,7 +2,6 @@
 using CarRentalApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Numerics;
 
 namespace CarRentalApi.Controllers;
 
@@ -19,8 +18,28 @@ public class ReportController : ControllerBase
         _context = context;
     }
 
+    [HttpGet("{id}", Name = "GetLastLogged")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LastLoggedReport))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetLastLoggedAsync(int id)
+    {
+        var lastLogged = await _context.LastLoggings.FirstOrDefaultAsync(x => x.Id == id);
+        return lastLogged is null ? NotFound() : Ok(lastLogged);
+    }
+
+
+    //[HttpGet("{id}", Name = "GetLastLogged2")]
+    //public async Task<LastLoggedReport> GetLastLoggedAsync2(int id)
+    //{
+    //    var lastLogged = await _context.LastLoggings.FirstOrDefaultAsync(x => x.Id == id);
+    //    return lastLogged;
+    //}
+
+
+
+
     [HttpGet(Name = "GetAllLastLogged")]
-    public async Task<IEnumerable<LastLoggedReport>> Get()
+    public async Task<IEnumerable<LastLoggedReport>> GetAllLastLogged()
     {
         var allLastLogged = await _context.LastLoggings.ToListAsync();
         return allLastLogged;
@@ -30,25 +49,42 @@ public class ReportController : ControllerBase
     public async Task AddAsync([FromBody] LastLoggedReport lastLoggedReport)
     {
         var existingRow = await _context.LastLoggings
-            .FirstOrDefaultAsync<LastLoggedReport>(x => x.UserId == lastLoggedReport.UserId);
+            .FirstOrDefaultAsync(x => x.UserId == lastLoggedReport.UserId);
 
         if (existingRow != null)
         {
-            _context.LastLoggings.Update(lastLoggedReport);
-            await _context.SaveChangesAsync();
-            return; 
+            await UpdateAsync(lastLoggedReport);
+            return;
         }
         await _context.AddAsync(lastLoggedReport);
         await _context.SaveChangesAsync();
     }
 
+    //[HttpPost(Name = "AddLastLogged2")]
+    //public async Task AddAsync2([FromBody] LastLoggedReport lastLoggedReport)
+    //{
+    //    var existingRow = await _context.LastLoggings
+    //        .FirstOrDefaultAsync(x => x.UserId == lastLoggedReport.UserId);
+    //    if (existingRow != null)
+    //    {
+    //        await UpdateAsync(lastLoggedReport);
+    //    }
+    //    else
+    //    {
+    //        await _context.AddAsync(lastLoggedReport);
+    //    }
+    //    await _context.SaveChangesAsync();
+    //}
+
     [HttpPut(Name = "UpdateLastLogged")]
-    public void Update([FromBody] LastLoggedReport lastLoggedReport)
+    public async Task UpdateAsync([FromBody] LastLoggedReport lastLoggedReport)
     {
-        //_context.LastLoggings.Update(lastLoggedReport);
-        //var lastLogged = _context.LastLoggings.Find(lastLoggedReport.Id);
-        var lastLogged = _context.LastLoggings.FirstOrDefault(x => x.UserId == lastLoggedReport.UserId);
-        lastLogged.LastLogged = lastLoggedReport.LastLogged;
-        _context.SaveChanges();
+        //var lastLogged = await _context.LastLoggings.FirstOrDefaultAsync(x => x.UserId == lastLoggedReport.UserId);
+        var lastLogged = await _context.LastLoggings.FindAsync(lastLoggedReport.UserId);
+        if (lastLogged != null)
+        {
+            lastLogged.LastLogged = lastLoggedReport.LastLogged;
+            await _context.SaveChangesAsync();
+        }
     }
 }
