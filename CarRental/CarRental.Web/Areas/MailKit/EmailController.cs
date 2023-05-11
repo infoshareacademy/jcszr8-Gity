79f90a2a2de35b12a69fd394f6b56748c102bb77
Jacek Pardel <jacekpardel@gmail.com>
2023-05-11 20:18:14 +0200

    smtp-client-configuration

---
+++ CarRental/CarRental.Web/Areas/MailKit/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeMessage
+
+
+namespace CarRental.Web.Areas.MailKit;
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult SendEmail(string body)
+    {
+        var email = new MimeMessage();
+
+    }
+}
