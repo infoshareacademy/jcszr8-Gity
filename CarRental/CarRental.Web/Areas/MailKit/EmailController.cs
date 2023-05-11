4a74e3b205e0b7e0a1bfa351831345e4b526df14
Jacek Pardel <jacekpardel@gmail.com>
2023-05-11 21:04:59 +0200

    full-configuration

--- CarRental/CarRental.Web/Areas/MailKit/EmailController.cs
+++ CarRental/CarRental.Web/Areas/MailKit/EmailController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MimeMessage
-
+using MimeKit;
 
 namespace CarRental.Web.Areas.MailKit;
 [Route("api/[controller]")]
@@ -12,6 +13,17 @@ public class EmailController : ControllerBase
     public IActionResult SendEmail(string body)
     {
         var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("team-gity@o2.pl"));
+        email.To.Add(MailboxAddress.Parse("jacekpardel@gmail.com"));
+        email.Subject = "Daily report";
+        email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+        smtp.Authenticate("team-gity@tlen.pl", "jcszr8-Gity");
+        smtp.Send(email);
+        smtp.Disconnect(true);
+        return Ok();
 
     }
 }
