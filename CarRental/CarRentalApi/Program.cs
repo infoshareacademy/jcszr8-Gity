59a709f1d4019f87963bb3839b8746e6ffc8036f
janek2j <janek2j@gmail.com>
2023-05-11 18:52:43 +0200

    add rest api project

---
+++ CarRental/CarRentalApi/Program.cs
@@ -0,0 +1,34 @@
+using CarRentalApi.Context;
+using Microsoft.EntityFrameworkCore;
+using CarRentalApi.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CarRentalApiContext>(options =>
+    options.UseSqlServer(builder.Configuration
+    .GetConnectionString("DefaultConnection"))
+);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
