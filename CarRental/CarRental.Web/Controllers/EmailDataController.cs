5b8748419c111962db2927c2486cc92743f69f4a
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-05-14 19:58:27 +0200

    changed to gmail

--- CarRental/CarRental.Web/Controllers/EmailDataController.cs
+++ CarRental/CarRental.Web/Controllers/EmailDataController.cs
@@ -1,4 +1,5 @@
-﻿using MailKit.Net.Smtp;
+﻿using System.Net.Mail;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
 using MimeKit;
 using CarRental.Web.Models;
@@ -26,8 +27,8 @@ namespace CarRental.Web.Controllers
             email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = emailData.Body };
 
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.ethereal.email", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            //smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //smtp.Connect("smtp.ethereal.email", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
             smtp.Authenticate(emailData.From, emailData.Password);
             smtp.Send(email);
             smtp.Disconnect(true);
