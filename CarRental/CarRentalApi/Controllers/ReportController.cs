3ee85627eaff13e9cca104004151515d79e3d1b1
janek2j <janek2j@gmail.com>
2023-05-14 18:35:20 +0200

    WIP

---
+++ CarRental/CarRentalApi/Controllers/ReportController.cs
@@ -0,0 +1,51 @@
+﻿using CarRentalApi.Context;
+using CarRentalApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace CarRentalApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReportController : ControllerBase
+{
+    private readonly CarRentalApiContext _context;
+    private readonly ILogger<ReportController> _logger;
+
+    public ReportController(ILogger<ReportController> logger, CarRentalApiContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet(Name = "GetAllLastLogged")]
+    public async Task<IEnumerable<LastLoggedReport>> Get()
+    {
+        var allLastLogged = await _context.LastLoggings.ToListAsync();
+        return allLastLogged;
+    }
+
+    [HttpPost(Name = "AddLastLogged")]
+    public async Task AddAsync([FromBody] LastLoggedReport lastLoggedReport)
+    {
+        var existingRow = await _context.LastLoggings
+            .FirstOrDefaultAsync<LastLoggedReport>(x => x.UserId == lastLoggedReport.UserId);
+
+        if (existingRow != null)
+        {
+            _context.LastLoggings.Update(lastLoggedReport);
+            await _context.SaveChangesAsync();
+            return; 
+        }
+        await _context.AddAsync(lastLoggedReport);
+        await _context.SaveChangesAsync();
+    }
+
+    [HttpPut(Name = "UpdateLastLogged")]
+    public async Task UpdateAsync([FromBody] LastLoggedReport lastLoggedReport)
+    {
+        _context.LastLoggings.Update(lastLoggedReport);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ CarRental/CarRentalApi/Models/LastLoggedReport.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalApi.Models;
+
+public class LastLoggedReport
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime LastLogged { get; set; }
+}
