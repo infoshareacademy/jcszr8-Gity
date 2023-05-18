bcaf9e4c6a6a635b1cb849d3270d5f2ee7790bf3
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-05-18 18:09:55 +0200

    added new service for posting user activity

---
+++ CarRental/CarRental.Logic/Services/IServices/IUserActivityService.cs
@@ -0,0 +1,8 @@
+﻿using CarRental.Logic.Models;
+
+namespace CarRental.Logic.Services.IServices;
+public interface IUserActivityService
+{
+    Task PostUserActivityAsync(VisitedCarDTO visitedCarDto,string apiEndpoint);
+    Task OnDetailsButtonClicked(CarViewModel visitedCar);
+}
