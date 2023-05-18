695571a9f81b576dd40b84d3109680a89425e1bf
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-05-18 14:18:12 +0200

    Create LastLoggedReport.cs

---
+++ CarRental/CarRental.DAL/Entities/LastLoggedReport.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.DAL.Entities;
+public class LastLoggedReport
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime LastLogged { get; set; }
+}
