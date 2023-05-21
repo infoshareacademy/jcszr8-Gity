f020d91224d02138f1c3c358f4051dde899abcee
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-05-21 18:26:54 +0200

    Update LastLoggedReport.cs

--- CarRental/CarRental.DAL/Entities/LastLoggedReport.cs
+++ CarRental/CarRental.DAL/Entities/LastLoggedReport.cs
@@ -5,7 +5,6 @@ using CarRental.DAL.Entities.BaseEntity;
 namespace CarRental.DAL.Entities;
 public class LastLoggedReport : Entity
 {
-    public int Id { get; set; }
     public int UserId { get; set; }
     public DateTime LastLogged { get; set; }
 }
