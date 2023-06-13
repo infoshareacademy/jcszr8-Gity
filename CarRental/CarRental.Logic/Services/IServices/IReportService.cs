0afa670dc9e126934b4ec7dfab862a35a272c8f0
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-06-13 14:19:06 +0200

    ogarnieci ReportServicu
    sprowadzilem pobieranie z api do jednej metody

--- CarRental/CarRental.Logic/Services/IServices/IReportService.cs
+++ CarRental/CarRental.Logic/Services/IServices/IReportService.cs
@@ -5,5 +5,6 @@ namespace CarRental.Logic.Services.IServices;
 public interface IReportService
 {
     Task ReportCarVisitAsync(CarViewModel visitedCar, string userId);
-    Task ReportUserLoginAsync(string email, int userId, DateTime from, DateTime to);
+    Task ReportUserLoginAsync(string email);
+    Task<IEnumerable<object>> GetReportsAsync(int userId, DateTime from, DateTime to, string reportType);
 }
--- CarRental/CarRental.Logic/Services/ReportService.cs
+++ CarRental/CarRental.Logic/Services/ReportService.cs
@@ -7,47 +7,25 @@ using CarRental.Logic.Services.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
 
 namespace CarRental.Logic.Services;
 public class ReportService : IReportService // ReportService
 {
-    private readonly HttpClient httpClient;
+    private readonly HttpClient _httpClient;
     private readonly IMapper _mapper;
     private readonly UserManager<Customer> _userManager;
     private readonly IRepository<VisitedCar> _visitedCarRepository;
     private readonly IRepository<LastLoggedReport> _lastLoggedReportRepository;
     public ReportService(IMapper mapper, UserManager<Customer> userManager, IRepository<VisitedCar> visitedCarRepository, IRepository<LastLoggedReport> lastLoggedReportRepository)
     {
-        httpClient = new HttpClient();
+        _httpClient = new HttpClient();
         _mapper = mapper;
         _userManager = userManager;
         _visitedCarRepository = visitedCarRepository;
         _lastLoggedReportRepository = lastLoggedReportRepository;
     }
 
-    private async Task<HttpResponseMessage> PostUserActivityAsync(object reportModel, string apiEndpoint)
-    {
-        var json = Newtonsoft.Json.JsonConvert.SerializeObject(reportModel);
-        var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-        return await httpClient.PostAsync(apiEndpoint, content);
-    }
-
-    private async Task<IEnumerable<VisitedCarViewModel>> GetFromApiAsync(string apiEndpoint, int userId, DateTime from, DateTime to)
-    {
-        using (var httpClient = new HttpClient())
-        {
-            var response = await httpClient.GetAsync(apiEndpoint);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var responsData = await response.Content.ReadAsStringAsync();
-                var visitedCars = JsonConvert.DeserializeObject<IEnumerable<VisitedCarViewModel>>(responsData);
-                return visitedCars;
-            }
-            return null;
-        }
-    }
 
     public async Task ReportCarVisitAsync(CarViewModel visitedCar, string userId)
     {
@@ -73,13 +51,6 @@ public class ReportService : IReportService // ReportService
         }
     }
 
-    public async Task<IEnumerable<VisitedCarViewModel>> GetVisitedCarsAsync(int userId, DateTime from, DateTime to)
-    {
-        var apiEndpoint = "https://localhost:7225/VisitedCar";
-        IEnumerable<VisitedCarViewModel> visitedCars = await GetFromApiAsync(apiEndpoint,userId, from,to);
-        return visitedCars;
-    }
-
     public async Task ReportUserLoginAsync(string email)
     {
         var userId = await GetUserIdAsync(email);
@@ -98,6 +69,48 @@ public class ReportService : IReportService // ReportService
             // Task logowanie zdarzeń aplikacji (dodać log.Error)
         }
     }
+
+    public async Task<IEnumerable<object>> GetReportsAsync(int userId, DateTime from, DateTime to,string reportType)
+    {
+        var apiEndpoint = reportType == "visitedCars"
+            ? "https://localhost:7225/VisitedCar"
+            : "https://localhost:7225/Report";
+        IEnumerable<object> visitedCars = await GetFromApiAsync(apiEndpoint,userId, from,to, reportType);
+        return visitedCars;
+    }
+
+    private async Task<HttpResponseMessage> PostUserActivityAsync(object reportModel, string apiEndpoint)
+    {
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(reportModel);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        return await _httpClient.PostAsync(apiEndpoint, content);
+    }
+
+    private async Task<IEnumerable<object>> GetFromApiAsync(string apiEndpoint, int userId, DateTime from, DateTime to,string reportType)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetAsync(apiEndpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responsData = await response.Content.ReadAsStringAsync();
+                if(reportType == "visitedCars")
+                {
+                    var visitedCars = JsonConvert.DeserializeObject<IEnumerable<VisitedCarViewModel>>(responsData);
+                    return visitedCars;
+                }
+                else if (reportType == "lastLogged")
+                {
+                    var lassLoggedReport = JsonConvert.DeserializeObject<IEnumerable<LastLoggedReportDTO>>(responsData);
+                }
+                
+            }
+            return null;
+        }
+    }
+
     private async Task<int> GetUserIdAsync(string email)
     {
         var user = _userManager.Users.FirstOrDefault(c => c.Email == email);
